acb976bf8c2749b353a0cd3347a15b38cae3da41
cxy <37500268+chenxygh@users.noreply.github.com>
2020-12-04 22:45:06 +0800

    05_01 not done

---
+++ 05_01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+    int n = 0;
+    scanf ("%d", &n);
+    int nums[n];
+    for (int i = 0; i < n; ++i) {
+        scanf ("%d", nums + i);
+    }
+    int dp[n] = {0};
+    for (int i = 0; i < n; ++i) {
+        dp[i] = 1;
+    }
+    for ()
+    return 0;
+}

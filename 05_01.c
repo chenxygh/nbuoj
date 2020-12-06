4eddc2641a7562cb84c79544c4beee613b8149a9
cxy <37500268+chenxygh@users.noreply.github.com>
2020-12-06 22:31:07 +0800

    to ubuntu

--- 05_01.c
+++ 05_01.c
@@ -1,23 +1,44 @@
 #include <stdio.h>
 
+#define INT_MIN -2147483648
+
+typedef struct {
+    int index;
+    int val;
+} Dp;
+
 int main () {
     int n = 0;
     scanf ("%d", &n);
-    int nums[n], res[210] = {0};
+    int nums[n];
     for (int i = 0; i < n; ++i) {
         scanf ("%d", nums + i);
     }
-    int dp[n] = {0};
+    Dp dp[n];
+    int index = 0, max = INT_MIN;
     for (int i = 0; i < n; ++i) {
-        dp[i] = 1;
-    }
-    for (int i = n - 2; i >= 0; --i) {
-        for (int j = n - 1; j > i; --j) {
-            if (nums[i] <= nums[j]) {
-                dp[i] = dp[j] + 1;
-
+        dp[i].index = i;
+        dp[i].val = 1;
+        for (int j = 1; j < i; ++j) {
+            if (nums[j] <= nums[i] && dp[j].val + 1 >= dp[i].val) {
+                dp[i].val = dp[j].val + 1;
+                dp[i].index = j;
             }
         }
+        if (dp[i].val >= max) {
+            max = dp[i].val;
+            index = i;
+        }
     }
+    int size = dp[index].val;
+    printf("%d\n%d", size, nums[index]);
+    --size;
+    index = dp[index].index;
+    while (size--) {
+        printf(" %d", nums[index]);
+        index = dp[index].index;
+    }
+    printf("\n");
+
     return 0;
 }

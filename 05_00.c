3a128f89a6b1c0fd0223f4b81dd532f136b93579
cxy <37500268+chenxygh@users.noreply.github.com>
2020-12-06 21:04:07 +0800

    05_01 undone, switch to sublime

--- 05_00.c
+++ 05_00.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+
 
 #define INT_MIN -2147483648
 
--- 05_01.c
+++ 05_01.c
@@ -3,7 +3,7 @@
 int main () {
     int n = 0;
     scanf ("%d", &n);
-    int nums[n];
+    int nums[n], res[210] = {0};
     for (int i = 0; i < n; ++i) {
         scanf ("%d", nums + i);
     }
@@ -11,6 +11,13 @@ int main () {
     for (int i = 0; i < n; ++i) {
         dp[i] = 1;
     }
-    for ()
+    for (int i = n - 2; i >= 0; --i) {
+        for (int j = n - 1; j > i; --j) {
+            if (nums[i] <= nums[j]) {
+                dp[i] = dp[j] + 1;
+
+            }
+        }
+    }
     return 0;
 }
